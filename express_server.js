7855a2859b1151e084890b59f5598e8043aa6523
Amanda Tuyen <atuyen26@gmail.com>
2021-03-18 12:19:46 +0000

    added urls new template and created a route in server

--- express_server.js
+++ express_server.js
@@ -5,7 +5,7 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
-
+  
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -17,6 +17,10 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
   
 })
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
