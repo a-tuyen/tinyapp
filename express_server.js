0939a1bb813ccdd241a2f254e39f75adeba81c75
Amanda Tuyen <atuyen26@gmail.com>
2021-03-18 11:52:12 +0000

    added route to look up indiv url in database and template for urls show page

--- express_server.js
+++ express_server.js
@@ -11,13 +11,17 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
-
-  console.log(templateVars);
   res.render('urls_index', templateVars);
   
 })
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
