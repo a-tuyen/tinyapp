de5d9771dbcc50e53b20b9b479a43ac01ba347b0
Amanda Tuyen <atuyen26@gmail.com>
2021-03-21 10:35:14 +0000

    added register page and route and updated header

--- express_server.js
+++ express_server.js
@@ -19,14 +19,58 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
 ////load index and table of our database
 app.get('/urls', (req, res) => {
   const templateVars = { 
-    username: req.cookies['user'],
+    user: users[req.cookies.userID],
     urls: urlDatabase };
   res.render('urls_index', templateVars);
-  
 })
+
+//load register page
+app.get('/register', (req, res) => {
+  const templateVars = { 
+    user: users[req.cookies.userID],
+    // email: users[req.cookies.userID].email,
+    urls: urlDatabase };
+  res.render('urls_register', templateVars);
+})
+
+app.post('/register', (req, res) => {
+  let userID = generateRandomString();
+  users[userID] = {
+    'id': userID, 
+    'email': req.body.email, 
+    'password': req.body.password
+  };
+  console.log('body', req.body);
+  console.log('users', users);
+  res.cookie('userID', userID);
+  console.log('reqcookies', req.cookies)
+  res.redirect('/urls');
+})
+
+
+//creates short URL and adds url submission to database
+app.post('/urls', (req, res) => {
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = 'http://www.' + req.body.longURL;
+  res.redirect('/urls/' + shortURL);
+});
+
 //creates cookie when logging in
 app.post('/login', (req, res) => {
   const username = req.body.username;
@@ -34,10 +78,11 @@ app.post('/login', (req, res) => {
   res.redirect('/urls');
 })
 
+//logs user out and clears cookie
 app.post('/logout', (req, res) => {
   res.clearCookie('user');
+  res.clearCookie('userID');
   res.redirect('/urls');
-
 })
 
 //delete entry in our database
@@ -56,7 +101,7 @@ app.post('/urls', (req, res) => {
 //load new page template/ create indiv url page
 app.get("/urls/new", (req, res) => {
   const templateVars = {
-    username: req.cookies['user'],
+    user: users[req.cookies.userID],
     urls: urlDatabase }
   res.render("urls_new", templateVars);
 });
@@ -72,7 +117,7 @@ app.post('/urls/:shortURL', (req, res) => {
 //loads indiv url page
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
-    username: req.cookies['user'],
+    user: users[req.cookies.userID],
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
